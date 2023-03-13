9c2c8ab1f678da94caa3d36592936bbc3ceda342
Jeremstym <jeremstym@gmail.com>
2023-03-13 14:28:56 +0100

    first commit

---
+++ bayesian_model.py
@@ -0,0 +1,127 @@
+import numpy as np 
+import numpy.random as nprd
+import numpy.linalg as nplg
+import scipy as sp 
+
+### Hyperparameters
+
+tau2 = 1
+r = 5
+beta_max = 4
+max_neighbors = 40 # be sure that there are more data than max_neighbors
+
+### Model 
+
+def model_conditionnal(y, X, beta, k):
+    return 1 
+
+def pseudo_conditional(y, X, beta, k):
+    
+    prod_dens = 1
+    for x_i, label_i in zip(X, y):
+        # k-nearest-neighbors of x_i
+        dist_x_i = nplg.norm(X-x_i, axis=1)
+        knn_xi_idx = dist_x_i.argsort()[:k+1] # takes itself into account
+        nearest_labels_to_i = y[knn_xi_idx[1:]]
+
+        # x_l's for which x_i is one of the knn
+        i_nearest_to_labels = []
+        for x_l, label in zip(X, y):
+            dist_x_l = nplg(X-x_l, axis=1)
+            knn_xl_idx = dist_x_l.argsort()[:k+1] # takes itself into account 
+            if dist_x_l[knn_xl_idx][-1] >= nplg.norm(x_l-x_i):
+                i_nearest_to_labels.append(label)
+
+        sum1 = np.sum([1 for label in nearest_labels_to_i if label == label_i]
+                      + [1 for label in i_nearest_to_labels if label == label_i])
+        
+        sum2 = np.sum([np.exp(beta/k*np.sum([1 for label in nearest_labels_to_i if label == g]))
+                      * np.exp(beta/k*np.sum([1 for label in i_nearest_to_labels if label == g]))
+                      for g in np.unique(y)])
+        
+        conditional_dens = np.exp(beta/k*sum1)/sum2
+        prod_dens *= conditional_dens    
+
+    return prod_dens
+
+def constant_approximation_mc(y, X, beta, k, niter=100):
+    def summation_given_y(y):
+        sum1 = 0
+        for x_i, label_i in zip(X, y):
+            # k-nearest-neighbors of x_i
+            dist_x_i = nplg.norm(X-x_i, axis=1)
+            knn_xi_idx = dist_x_i.argsort()[:k+1] # takes itself into account
+            nearest_labels_to_i = y[knn_xi_idx[1:]]
+            sum1 += np.sum([1 for label in nearest_labels_to_i if label == label_i])/k
+        return sum1
+    
+    nb_labels = len(np.unique(y))
+    list_y = np.array(np.meshgrid(*[np.unique(y) for i in range(nb_labels)])).T.reshape(-1,nb_labels)
+    
+    def summation_given_u(u):
+        sum_y = 0
+        for y in list_y:
+            sum1 = summation_given_y(y)
+            sum_y += np.exp(u*sum1)
+        return sum_y
+    
+    def expectation_given_u(u):
+        normalization = summation_given_u(u)
+        sum_expect = 0
+        for y in list_y:
+            sum1 = summation_given_y(y)
+            sum_expect += sum1 * np.exp(u*sum1)/normalization
+        return sum_expect
+    
+    # Monte Carlo integration
+
+    mc_sum = 0
+    for iter in range(niter):
+        u = nprd.random()*beta
+        mc_sum += beta*expectation_given_u(u)/niter
+
+    log_result = np.log(len(y))*2*mc_sum
+    return np.exp(log_result)
+
+
+def jacobian(theta):
+    return np.exp(theta)/((1+np.exp(theta))**2)
+
+def uniform_k(k_old):
+    k_up = [i for i in range(k_old, k_old+r) if i <= max_neighbors]
+    k_down = [i for i in range(k_old - r, k_old) if i >= 0]
+    k_new = nprd.choice(k_down+k_up)
+
+    return k_new, len(k_down+k_up)
+
+def acceptance_threshold(y, X, theta_new, theta_old, k_new, k_old):
+    beta_new = beta_max*np.exp(theta_new)/(1+np.exp(theta_new))
+    beta_old = beta_max*np.exp(theta_old)/(1+np.exp(theta_old))
+    prob_knew = 1/uniform_k(k_old)[1]
+    prob_kold = 1/uniform_k(k_new)[1]
+    threshold = (model_conditionnal(y, X, beta_new, k_new)\
+                    *jacobian(theta_new))*prob_kold\
+                        /(model_conditionnal(y, X, beta_old, k_new)\
+                          *jacobian(theta_old)*prob_knew)
+
+    return threshold
+
+def metropolis_hastings(y, X, niter, burning=None):
+    k_old = nprd.randint(max_neighbors+1)
+    theta_old = nprd.normal(0, tau2)
+
+    for iter in range(niter):
+        
+        k_new = uniform_k(k_old)[0]
+        theta_new = nprd.normal(theta_old, tau2)
+
+        u = nprd.random()
+        acceptance = acceptance_threshold(y, X, theta_new, theta_old, k_new, k_old)
+        if acceptance > u:
+            k_old = k_new
+            theta_old = theta_new
+
+    beta_final = beta_max*np.exp(theta_old)/(1+np.exp(theta_old))
+    k_final = k_old
+
+    return beta_final, k_final 
---
+++ knn_baseline.py
@@ -0,0 +1,32 @@
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import seaborn as sns
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+from sklearn.inspection import DecisionBoundaryDisplay
+
+n_neighbors = 15
+
+# import some data to play with
+iris = datasets.load_iris()
+
+data = iris.data
+target = iris.target
+
+for x, y in zip(data, target):
+    print(x)
+    print(y)
+    break
+
+x = np.array([5.4, 3.3, 7.1, 1.1])
+
+np.linalg.norm(data-x, axis=1).argsort()[:3]
+
+for x_l in data:
+    np.linalg.norm(data - x_l, axis=1)
+
+np.linalg.norm(data-x, axis=1)
+
