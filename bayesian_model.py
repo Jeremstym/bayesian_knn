50fc4f05c75ae417762add64a2cd5ec2ff2e2789
Jeremstym <jeremstym@gmail.com>
2023-03-16 14:12:15 +0100

    Add z_sampling method

--- bayesian_model.py
+++ bayesian_model.py
@@ -1,24 +1,20 @@
-import numpy as np 
+import numpy as np
 import numpy.random as nprd
 import numpy.linalg as nplg
-import scipy as sp 
-
 from tqdm import tqdm
+from scipy.stats import boltzmann
 
 ### Hyperparameters
 
 tau2 = 1
-r = 5
-beta_max = 4
+r = 5  
+beta_max = 100
 max_neighbors = 80 # be sure that there are more data than max_neighbors
 
-### Model 
-
-def model_conditionnal(y, X, beta, k):
-    return 1 
+# ----------------------------- Model -------------------------------------
 
 def pseudo_conditional(y, X, beta, k):
-    
+
     prod_dens = 1
     for x_i, label_i in zip(X, y):
         # k-nearest-neighbors of x_i
@@ -30,23 +26,23 @@ def pseudo_conditional(y, X, beta, k):
         i_nearest_to_labels = []
         for x_l, label in zip(X, y):
             dist_x_l = nplg.norm(X-x_l, axis=1)
-            knn_xl_idx = dist_x_l.argsort()[:k+1] # takes itself into account 
+            knn_xl_idx = dist_x_l.argsort()[:k+1] # takes itself into account
             if dist_x_l[knn_xl_idx][-1] >= nplg.norm(x_l-x_i):
                 i_nearest_to_labels.append(label)
 
         sum1 = np.sum([1 for label in nearest_labels_to_i if label == label_i]
                       + [1 for label in i_nearest_to_labels if label == label_i])
-        
+
         sum2 = np.sum([np.exp(beta/k*np.sum([1 for label in nearest_labels_to_i if label == g]))
                       * np.exp(beta/k*np.sum([1 for label in i_nearest_to_labels if label == g]))
                       for g in np.unique(y)])
-        
+
         conditional_dens = np.exp(beta/k*sum1)/sum2
-        prod_dens *= conditional_dens    
+        prod_dens *= conditional_dens
 
     return prod_dens
 
-def constant_ratio_approximation_mc(y, X, beta, k, niter=100):
+def constant_ratio_approximation_mc(y, X, beta, k, mc_iter=100):
     def summation_given_y(y):
         sum1 = 0
         for x_i, y_i in zip(X, y):
@@ -56,36 +52,61 @@ def constant_ratio_approximation_mc(y, X, beta, k, niter=100):
             nearest_labels_to_i = y[knn_xi_idx[1:]]
             sum1 += np.sum([1 for label in nearest_labels_to_i if label == y_i])/k
         return sum1
-    
+
+    size = len(y)
     nb_labels = len(np.unique(y))
-    list_y = np.array(np.meshgrid(*[np.unique(y) for i in range(nb_labels)])).T.reshape(-1,nb_labels)
-    
+    size_iteration = 10
+
     def summation_given_u(u):
+        # Summing over all y's possible is too costy, we select some of them randomly instead
         sum_y = 0
-        for y_sample in list_y:
+        for y_sample in range(size_iteration):
+            y_sample = nprd.randint(0, nb_labels, size)
             sum1 = summation_given_y(y_sample)
             sum_y += np.exp(u*sum1)
         return sum_y
-    
+
     def expectation_given_u(u):
+        # Summing over all y's possible is too costy, we select some of them randomly instead
         normalization = summation_given_u(u)
         sum_expect = 0
-        for y_sample in list_y:
+        for y_sample in range(size_iteration):
+            y_sample = nprd.randint(0, nb_labels, size)
             sum1 = summation_given_y(y_sample)
             sum_expect += sum1 * np.exp(u*sum1)/normalization
         return sum_expect
-    
+
     # Monte Carlo integration
     mc_sum = 0
-    for iter in range(niter):
+    for iter in range(mc_iter):
         u = nprd.random()*beta
-        mc_sum += beta*expectation_given_u(u)/niter
+        mc_sum += beta*expectation_given_u(u)/mc_iter
 
     normalization = np.exp(len(y)*np.log(2)+mc_sum)
     density = np.exp(beta*summation_given_y(y))/normalization
-    
+
     return density
 
+def z_sampling(y, X, beta_hat, k_hat, beta, k):
+    
+    def summation_given_target(z):
+        sum1 = 0
+        for x_i, z_i in zip(X, z):
+            # k-nearest-neighbors of x_i
+            dist_x_i = nplg.norm(X-x_i, axis=1)
+            knn_xi_idx = dist_x_i.argsort()[:k+1] # takes itself into account
+            nearest_labels_to_i = z[knn_xi_idx[1:]]
+            sum1 += np.sum([1 for label in nearest_labels_to_i if label == z_i])/k
+        return sum1
+
+    nb_labels = len(np.unique(y))
+    z = boltzmann.rvs(beta/k, N=nb_labels, size=len(y))
+
+    # No need for normalization because of the ratio in MH algorithm
+    g_dens = np.exp(beta_hat/k_hat*summation_given_target(z))
+
+    return np.exp(beta/k*summation_given_target(y))*g_dens/np.exp(beta/k*summation_given_target(z))
+    
 
 def jacobian(theta):
     return np.exp(theta)/((1+np.exp(theta))**2)
@@ -105,25 +126,40 @@ def acceptance_threshold(y, X, theta_new, theta_old, k_new, k_old, method):
     beta_old = beta_max*np.exp(theta_old)/(1+np.exp(theta_old))
 
     if method =='pseudo_likelihood':
-        model_conditionnal_up = pseudo_conditional(y, 
-                                                   X, 
-                                                   beta_new, 
+        model_conditionnal_up = pseudo_conditional(y,
+                                                   X,
+                                                   beta_new,
                                                    k_new)
-        model_conditionnal_down = pseudo_conditional(y, 
-                                                     X, 
-                                                     beta_old, 
+        model_conditionnal_down = pseudo_conditional(y,
+                                                     X,
+                                                     beta_old,
                                                      k_old)
 
     elif method == 'path_sampling':
-        model_conditionnal_up = constant_ratio_approximation_mc(y, 
-                                                                X, 
-                                                                beta_new, 
+        model_conditionnal_up = constant_ratio_approximation_mc(y,
+                                                                X,
+                                                                beta_new,
                                                                 k_new)
-        model_conditionnal_down = constant_ratio_approximation_mc(y, 
-                                                                  X, 
-                                                                  beta_old, 
+        model_conditionnal_down = constant_ratio_approximation_mc(y,
+                                                                  X,
+                                                                  beta_old,
                                                                   k_old)
-    
+    elif method == 'z_sampling':
+        model_conditionnal_up = z_sampling(y,
+                                           X,
+                                           1.45,
+                                           15,
+                                           beta_new,
+                                           k_new)
+        
+        model_conditionnal_down = z_sampling(y,
+                                             X,
+                                             1.45,
+                                             15,
+                                             beta_old,
+                                             k_old)
+
+
     prob_knew = 1/uniform_k(k_old, r=r)[1]
     prob_kold = 1/uniform_k(k_new, r=r)[1]
     threshold = (model_conditionnal_up*jacobian(theta_new))*prob_kold\
@@ -131,10 +167,8 @@ def acceptance_threshold(y, X, theta_new, theta_old, k_new, k_old, method):
 
     return threshold
 
-def metropolis_hastings(y, X, niter, method, burning=1000):
+def metropolis_hastings(y, X, niter, method):
 
-    if burning >= niter:
-        burning = 0
 
     k_old = nprd.randint(max_neighbors+1)
     theta_old = nprd.normal(0, tau2)
@@ -144,13 +178,13 @@ def metropolis_hastings(y, X, niter, method, burning=1000):
         k_new = uniform_k(k_old)[0]
         theta_new = nprd.normal(theta_old, tau2)
         u = nprd.random()
-        
-        acceptance = acceptance_threshold(y, 
-                                          X, 
-                                          theta_new, 
-                                          theta_old, 
-                                          k_new, 
-                                          k_old, 
+
+        acceptance = acceptance_threshold(y,
+                                          X,
+                                          theta_new,
+                                          theta_old,
+                                          k_new,
+                                          k_old,
                                           method)
         if acceptance > u:
             theta_old = theta_new
@@ -171,28 +205,41 @@ def proba_with_params(g, x_new, y, X, beta, k):
     new_nearest_to_labels = []
     for x_l, label in zip(X, y):
         dist_x_l = nplg.norm(X-x_l, axis=1)
-        knn_xl_idx = dist_x_l.argsort()[:k+1] # takes itself into account 
+        knn_xl_idx = dist_x_l.argsort()[:k+1] # takes itself into account
         if dist_x_l[knn_xl_idx][-1] >= nplg.norm(x_l-x_new):
             new_nearest_to_labels.append(label)
 
     sum1 = np.sum([1 for label in nearest_labels_to_new if label == g]
                     + [1 for label in new_nearest_to_labels if label == g])
-    
+
     normalization = np.sum([np.exp(beta/k*np.sum([1 for label in nearest_labels_to_new if label == g]))
                     * np.exp(beta/k*np.sum([1 for label in new_nearest_to_labels if label == g]))
                     for g in np.unique(y)])
-    
+
     conditional_dens = np.exp(beta/k*sum1)/normalization
 
     return conditional_dens
 
 def proba_class(g, x_new, y, X, param_list):
-    
+
     M = len(param_list)
     proba_sum = 0
     for params in param_list:
         proba_sum += proba_with_params(g, x_new, y, X, *params)
-    
+
     proba_sum /= M
 
     return proba_sum
+
+def sanity_check(X, y, param_list):
+    accuracy = 0
+    idx = 0
+    for x_i in tqdm(X):
+        pred = np.argmax([proba_class(g, x_i, y, X, param_list) for g in np.unique(y)])
+        if pred == y[idx]:
+            accuracy += 1
+        idx += 1
+    
+    accuracy /= len(y)
+    print(f"Accuracy: {(100*accuracy):0.1f}%")
+    return None
