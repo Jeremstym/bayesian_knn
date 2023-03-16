97a7b76a02caa5e54fec400023f95dc9f9111f3f
Jeremstym <jeremstym@gmail.com>
2023-03-16 21:21:34 +0100

    Clean knn

--- knn_baseline.py
+++ knn_baseline.py
@@ -16,19 +16,3 @@ iris = datasets.load_iris()
 data = iris.data
 target = iris.target
 
-for x, y in zip(data, target):
-    print(x)
-    print(y)
-    break
-
-x = np.array([5.4, 3.3, 7.1, 1.1])
-
-np.linalg.norm(data-x, axis=1).argsort()[:3]
-
-for x_l in data:
-    np.linalg.norm(data - x_l, axis=1)
-
-np.linalg.norm(data-x, axis=1)
-
-np.unique(target)
-np.array(np.meshgrid(*[np.unique(target) for i in range(len(np.unique(target)))])).T.reshape(-1,3)
